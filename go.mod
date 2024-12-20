10fc1e059e021669cb920aa073405f1adf30dbd7
Petar <petar.kovacovski@gmail.com>
2024-12-20 11:31:28 +0100

    REPL Loop done, reading commands

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PetarKovacovski/pokedex
+
+go 1.23.3
---
+++ repl_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+func TestCleanInput(t *testing.T){
+	
+	cases := []struct{
+		input		string
+		expected	[]string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: " Charmander  Bulbasaur  PIKACHU  ",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for _, tc := range cases{
+		actual := cleanInput(tc.input)
+
+		for i := range actual{
+			if tc.expected[i] != actual[i]{
+				t.Errorf("Words don't match")
+			}
+		}
+
+	}
+
+
+}
